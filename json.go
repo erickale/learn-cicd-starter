4811bef828b0b5ace6c653bc1f7ab750723f7dfb
Eric Kale <the.primeagen@aol.com>
2025-04-16 09:00:34 -0500

    trigger5

--- json.go
+++ json.go
@@ -22,18 +22,17 @@ func respondWithError(w http.ResponseWriter, code int, msg string, logErr error)
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-    w.Header().Set("Content-Type", "application/json")
-    dat, err := json.Marshal(payload)
-    if err != nil {
-        log.Printf("Error marshalling JSON: %s", err)
-        w.WriteHeader(http.StatusInternalServerError)
-        return
-    }
-    w.WriteHeader(code)
-    _, err = w.Write(dat)
-    if err != nil {
-        log.Printf("Error writing response: %s", err)
-        // No further action possible here; headers already sent.
-    }
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(code)
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Error writing response: %s", err)
+		// No further action possible here; headers already sent.
+	}
 }
-
